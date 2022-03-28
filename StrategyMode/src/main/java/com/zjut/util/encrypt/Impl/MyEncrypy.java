fdb893d260d24609896d57c88c8f5d0a38c2633d
张坤 <2553150278@qq.com>
2022-03-28 11:40:01 +0800

    修改自定义加密方式

--- StrategyMode/src/main/java/com/zjut/util/encrypt/Impl/MyEncrypy.java
+++ StrategyMode/src/main/java/com/zjut/util/encrypt/Impl/MyEncrypy.java
@@ -3,6 +3,8 @@ package com.zjut.util.encrypt.Impl;
 import com.zjut.common.MyFile;
 import com.zjut.util.encrypt.EncryptStrategy;
 
+import java.util.Random;
+
 /**
  * @Author: 张坤
  * @DateTime: 2022/3/24 22:17
@@ -11,11 +13,21 @@ import com.zjut.util.encrypt.EncryptStrategy;
 public class MyEncrypy implements EncryptStrategy {
     @Override
     public MyFile encryptBytes(byte[] bytes) {
-        return new MyFile(EncryptTypeEnum.MyEncrypy,bytes);
+        byte key= (byte) (new Random().nextInt(128));
+        byte[] result =new byte[bytes.length];
+        for(int i=0;i<bytes.length;i++){
+            result[i]= (byte) (bytes[i]+key);
+        }
+        return new MyFile(EncryptTypeEnum.MyEncrypy,result,key);
     }
 
     @Override
     public byte[] decryptBytes(MyFile myFile) {
-        return myFile.getFileTxt();
+        byte key= (byte) myFile.getKey();
+        byte[] result =new byte[myFile.getFileTxt().length];
+        for(int i=0;i<result.length;i++){
+            result[i]= (byte) (myFile.getFileTxt()[i]-key);
+        }
+        return result;
     }
 }
